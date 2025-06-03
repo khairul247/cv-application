3a3859830225586ffc94bc591c1b195e231d335c
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2025-06-03 21:20:02 +0800

    Add related work experience section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,15 +5,24 @@ import styles from '../styles/Content.module.css'
 
 export default function Content() {
     const [formData, setFormData] = useState({
-        name: 'Harry James Potter',
-        phone: '+44 (0) 207-946-0958',
-        email: 'h.potter@hogwarts.edu',
-        university: 'Hogwarts School of Witchcraft and Wizardry',
-        degree: 'Certificate in Defense Against the Dark Arts (CGPA 3.97)',
-        uniLocation: 'Highlands, Scotland',
-        gradMonth: 'June',
-        gradYear:'2024',
-        relCourseWorks:'Advanced Transfiguration, Potions Theory and Practice, Charms and Enchantments, Herbology Applications, Care of Magical Creatures, Ancient Runes, Arithmancy, Defense Against the Dark Arts Mastery, Advanced Dueling Techniques.',
+        name: '',
+        phone: '',
+        email: '',
+        university: '',
+        degree: '',
+        uniLocation: '',
+        gradMonth: '',
+        gradYear:'',
+        relCourseWorks:'',
+        organization:'',
+        jobLocation:'',
+        jobTitle:'',
+        startMonth: '',
+        startYear:'',
+        endMonth: '',
+        endYear: '',
+        isCurrentJob: '',
+        responsibilities: '',
 
     })
 
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -7,33 +7,78 @@ export default function Preview({formData}) {
                 <article className={styles.resume}>
                     <header className={styles.resumeHeader}>
                         <h1 className={styles.name}>
-                            {formData.name}
+                            {formData.name || 'Harry James Potter'}
                         </h1>
                         <div className={styles.contactInfor}>
-                            <span>{formData.phone}</span>
+                            <span>{formData.phone || '+44 (0) 207-946-0958'}</span>
                             <span className={styles.separator}> | </span>
-                            <span>{formData.email}</span>
+                            <span>{formData.email || 'h.potter@hogwarts.edu'}</span>
                         </div>
                     </header>
-                    <div className='educationSection'>
+                    <div className={styles.newSection}>
                         <div className={styles.sectionHeader}>EDUCATION</div>
                         <div className='eduInfo'>
                             <div className={styles.rowEdu}> 
-                                <span> <b> {formData.university} </b></span>
-                                <span>{formData.uniLocation}</span>
+                                <span> <b> {formData.university || 'Hogwarts School of Witchcraft and Wizardry'} </b></span>
+                                <span>{formData.uniLocation || 'Highlands, Scotland'}</span>
                             </div>
 
                             <div className={styles.rowEdu}> 
-                                <span> <b>{formData.degree}</b></span>
-                                <span>{formData.gradMonth+' '+formData.gradYear}</span> 
+                                <span> <b>{formData.degree || 'Nastily Exhausting Wizarding Tests (N.E.W.T.s), 5(O)'}</b></span>
+                                <span>
+                                    {(formData.gradMonth && formData.gradYear) ? 
+                                        `${formData.gradMonth} ${formData.gradYear}` : 
+                                        "June 2024"
+                                    }
+                                </span>
                             </div>
                             
                             <div> 
                                 <span>Relevant courseworks: </span>
-                                <span>{formData.relCourseWorks}</span>
+                                <span>{formData.relCourseWorks || 'Advanced Transfiguration, Potions Theory and Practice, Charms and Enchantments, Herbology Applications, Care of Magical Creatures, Ancient Runes, Arithmancy, Defense Against the Dark Arts Mastery, Advanced Dueling Techniques.'}</span>
                             </div>
                         </div>
                     </div>
+
+                    <div className={styles.newSection}>
+                        <div className={styles.sectionHeader}>RELATED WORK EXPERIENCES</div>
+                        <div className='workInfo'>
+                            <div className={styles.rowEdu}> 
+                                <span> <b> {formData.organization || 'Department of Magical Accidents and Catastrophes'} </b></span>
+                                <span>{formData.jobLocation || 'Ministry of Magic'}</span>
+                            </div>
+
+                            <div className={styles.rowEdu}> 
+                                <span> <i>{formData.jobTitle || 'Auror Trainee'}</i></span>
+                                <span>
+                                    {(formData.startMonth && formData.startYear && formData.endMonth && formData.endYear) ? 
+                                        `${formData.startMonth} ${formData.startYear} - ${formData.endMonth} ${formData.endYear}` : 
+                                        "June 2024 - May 2025"
+                                    }
+                                </span>
+                            </div>
+                            
+                            <div>
+                            {(formData.responsibilities) ? 
+                                formData.responsibilities.split('\n').map((line, index) => (
+                                <div key={index} style={{ marginBottom: '3px' }}>
+                                    {line}
+                                </div>
+                                )) :
+                                [
+                                "• Led specialized task force in neutralizing 15+ dark magic incidents across Greater London area",
+                                "• Collaborated with Senior Aurors to develop new protective ward systems, improving department safety protocols by 40%",
+                                "• Mentored 8 junior wizards in advanced defensive spell casting and emergency response procedures"
+                                ].map((point, index) => (
+                                <div key={index} style={{ marginBottom: '3px' }}>
+                                    {point}
+                                </div>
+                                ))
+                            }
+                            </div>
+                        </div>
+                    </div>
+                    
                 </article>
         </section>
     )
